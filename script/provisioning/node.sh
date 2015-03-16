b333b27a98d9c3425b508224f82eaf05e1d95bf7
José Padilla <jpadilla@webapplicate.com>
2015-03-16 12:15:34 -0400

    Update node.sh

--- script/provisioning/node.sh
+++ script/provisioning/node.sh
@@ -1,21 +1,15 @@
-#!/usr/bin/env bash
-
-# Exit script immediately on first error.
-set -e
-
-# Print commands and their arguments as they are executed.
-set -x
+#!/bin/bash -eux
 
 SSH_USER=${SSH_USERNAME:-vagrant}
 
 export HOME=/home/${SSH_USER}
 
-# Install nvm
+echo "==> Installing nvm"
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 echo "source ~/.nvm/nvm.sh" >> /home/${SSH_USER}/.bashrc
 source /home/${SSH_USER}/.nvm/nvm.sh
 
-# Install latest stable Node.js version
+echo "==> Installing latest stable Node.js"
 nvm install stable
 nvm alias default stable
 
