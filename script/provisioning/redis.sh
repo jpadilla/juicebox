f941c73b4d7f06500c9973afe269441215e74527
José Padilla <jpadilla@webapplicate.com>
2015-03-16 12:17:29 -0400

    Update redis.sh

--- script/provisioning/redis.sh
+++ script/provisioning/redis.sh
@@ -1,18 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash -eux
 
-# Exit script immediately on first error.
-set -e
-
-# Print commands and their arguments as they are executed.
-set -x
-
-# Install redis-server
+echo "==> Installing redis"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv C7917B12
 sudo echo 'deb http://ppa.launchpad.net/chris-lea/redis-server/ubuntu precise main' >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y redis-server
 
-# Configure redis-server
+echo "==> Configuring redis"
 sudo sed -i 's/bind 127.0.0.1/# bind 127.0.0.1/g' /etc/redis/redis.conf
 sudo echo 'vm.overcommit_memory = 1' | sudo tee -a /etc/sysctl.conf
 sudo sysctl vm.overcommit_memory=1
