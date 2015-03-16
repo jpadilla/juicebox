84b256f05f9789c468ed98bc1a33effe55e434d3
José Padilla <jpadilla@webapplicate.com>
2015-03-16 12:16:01 -0400

    Update postgresql.sh

--- script/provisioning/postgresql.sh
+++ script/provisioning/postgresql.sh
@@ -1,15 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash -eux
 
-# Exit script immediately on first error.
-set -e
-
-# Print commands and their arguments as they are executed.
-set -x
-
-# Install postgresql
+echo "==> Installing postgresql"
 sudo apt-get -y install libpq-dev postgresql
 
-# Configure postgresql
+echo "==> Configuring postgresql"
 echo "listen_addresses = '*'" | sudo -u postgres tee -a /etc/postgresql/9.3/main/postgresql.conf
 echo "host all all 0.0.0.0/0 md5" | sudo -u postgres tee -a /etc/postgresql/9.3/main/pg_hba.conf
 sudo service postgresql restart
