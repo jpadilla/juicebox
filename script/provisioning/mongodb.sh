a1974a66615f61b63afd86f836a59b2ad4e1157a
José Padilla <jpadilla@webapplicate.com>
2015-03-16 12:14:55 -0400

    Update mongodb.sh

--- script/provisioning/mongodb.sh
+++ script/provisioning/mongodb.sh
@@ -1,12 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash -eux
 
-# Exit script immediately on first error.
-set -e
-
-# Print commands and their arguments as they are executed.
-set -x
-
-# Install MongoDB
+echo "==> Installing mongodb"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
