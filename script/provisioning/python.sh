2f1f0bfd1c61cf5ebee49c748f495380769b05c1
José Padilla <jpadilla@webapplicate.com>
2015-03-16 12:16:57 -0400

    Update python.sh

--- script/provisioning/python.sh
+++ script/provisioning/python.sh
@@ -1,22 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash -eux
 
-# Exit script immediately on first error.
-set -e
-
-# Print commands and their arguments as they are executed.
-set -x
-
-# Install Python 2
+echo "==> Installing Python 2.7"
 sudo add-apt-repository -y ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install -y python python-dev
 
-# Install setuptools
+echo "==> Installing setuptools"
 sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
 sudo python ez_setup.py
 
-# Install pip
+echo "==> Installing pip"
 sudo easy_install pip
 
 # TODO: Setup virtualenvwrapper
+echo "==> Installing virtualenvwrapper"
 sudo pip install virtualenvwrapper
