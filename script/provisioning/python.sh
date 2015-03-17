9c1c734ad992b7d5490b680b0a4daf90bbefefec
José Padilla <jpadilla@webapplicate.com>
2015-03-17 09:59:37 -0400

    Closes #3

--- script/provisioning/python.sh
+++ script/provisioning/python.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+SSH_USER=${SSH_USERNAME:-vagrant}
+
+export HOME=/home/${SSH_USER}
+
 echo "==> Installing Python 2.7"
 sudo add-apt-repository -y ppa:fkrull/deadsnakes
 sudo apt-get update
@@ -8,10 +12,18 @@ sudo apt-get install -y python python-dev
 echo "==> Installing setuptools"
 sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
 sudo python ez_setup.py
+rm ez_setup.py setuptools-*
 
 echo "==> Installing pip"
 sudo easy_install pip
 
-# TODO: Setup virtualenvwrapper
 echo "==> Installing virtualenvwrapper"
 sudo pip install virtualenvwrapper
+
+echo "==> Configuring virtualenvwrapper"
+mkdir -p ${HOME}/.virtualenvs
+echo "WORKON_HOME=${HOME}/.virtualenvs" >> ${HOME}/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ${HOME}/.bashrc
+
+chown -R ${SSH_USER}:${SSH_USER} ${HOME}/.virtualenvs
+export HOME=/home/root
