1f4eae5f09d7d3adbc8803aa6c8916d6c69c6797
José Padilla <jpadilla@webapplicate.com>
2015-04-01 11:27:32 -0400

    Update docker.sh

--- scripts/provisioning/docker.sh
+++ scripts/provisioning/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eux
 
+SSH_USER=${SSH_USERNAME:-vagrant}
+
 echo "==> Installing Docker"
 wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker juicebox
+sudo usermod -aG docker ${SSH_USER}
