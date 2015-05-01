2d3cd98d8bfe57c1003909792ecf93ee164959e3
José Padilla <jpadilla@webapplicate.com>
2015-05-01 11:27:14 -0400

    Tweaks to python provisioning script

--- scripts/provisioning/python.sh
+++ scripts/provisioning/python.sh
@@ -4,19 +4,28 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 
 export HOME=/home/${SSH_USER}
 
-echo "==> Installing Pyenv build tools"
+echo "==> Installing pyenv build tools"
 # From https://github.com/yyuu/pyenv/wiki/Common-build-problems
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm
+sudo apt-get install -y \
+    make \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm
 
-echo "==> Preparing Pyenv configuration."
+echo "==> Preparing pyenv configuration."
 cat <<EOF >> /home/${SSH_USER}/.bashrc
 export PATH="\$HOME/.pyenv/bin:\$PATH"
 eval "\$(pyenv init -)"
 eval "\$(pyenv virtualenv-init -)"
 EOF
 
-echo "==> Installing Pyenv"
+echo "==> Installing pyenv"
 su ${SSH_USER} <<EOF
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 source ~/.bashrc
@@ -26,3 +35,4 @@ source ~/.bashrc
 EOF
 
 chown -R ${SSH_USER}:${SSH_USER} /home/${SSH_USER}
+export HOME=/home/root
