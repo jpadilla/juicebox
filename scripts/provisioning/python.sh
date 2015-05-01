247cc1114c615123c96ed8ee357dbd7b4248c29e
Nelson Chen <crazysim@gmail.com>
2015-04-30 19:02:59 -0700

    Just use one echo

--- scripts/provisioning/python.sh
+++ scripts/provisioning/python.sh
@@ -2,17 +2,17 @@
 
 SSH_USER=${SSH_USERNAME:-vagrant}
 
-export HOME=/home/${SSH_USER}
-
 echo "==> Installing Pyenv build tools"
 # From https://github.com/yyuu/pyenv/wiki/Common-build-problems
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm
 
 echo "==> Preparing Pyenv configuration."
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> /home/${SSH_USER}/.bashrc
-echo 'eval "$(pyenv init -)"' >> /home/${SSH_USER}/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> /home/${SSH_USER}/.bashrc
+cat <<EOF >> /home/${SSH_USER}/.bashrc
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "\$(pyenv init -)"
+eval "\$(pyenv virtualenv-init -)"
+EOF
 
 su ${SSH_USER} <<EOF
 echo "==> Installing Pyenv"
